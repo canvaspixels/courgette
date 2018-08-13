8665775f17108dcac2c4b5c865f2f9b90c909c24
Alex Rogers <alex.rogers.ctr@lastminute.com>
2018-08-13 22:01:56 +0100

    lint fixes

--- scripts/add-script-to-packagejson.js
+++ scripts/add-script-to-packagejson.js
@@ -1,21 +1,21 @@
 const jsonfile = require('jsonfile');
 const path = require('path');
 
-function scriptAlreadyExists (name) {
-  this.name = 'scriptAlreadyExists'
-  this.message = 'My friend:\n  it seems as though the script entry you have specified,\n  "' + name + '"\n  is already present in the package.json file,\n  of your current working directory.\n  Please try again,\n  or amend this egregious error.\nThank You.'
+function scriptAlreadyExists(name) {
+  this.name = 'scriptAlreadyExists';
+  this.message = `${name} is already present in the package.json file.`;
 }
 
-scriptAlreadyExists.prototype = new Error()
-scriptAlreadyExists.prototype.constructor = scriptAlreadyExists
+scriptAlreadyExists.prototype = new Error();
+scriptAlreadyExists.prototype.constructor = scriptAlreadyExists;
 
-function noPackageJsonExists (name) {
-  this.name = 'noPackageJsonExists'
-  this.message = 'My friend:\n  it seems as though your current working directory\n  does not contain a package.json file,\n  and is therefore not a Node.js project.\n  Please run npm init,\n  in order to amend this tremendous violation.\nThank You.'
+function NoPackageJsonExists() {
+  this.name = 'NoPackageJsonExists';
+  this.message = 'No package.json present';
 }
 
-noPackageJsonExists.prototype = new Error()
-noPackageJsonExists.prototype.constructor = noPackageJsonExists
+NoPackageJsonExists.prototype = new Error();
+NoPackageJsonExists.prototype.constructor = NoPackageJsonExists;
 
 const packageJsonFilePath = path.join(process.cwd(), 'package.json');
 
@@ -23,15 +23,15 @@ try {
   const key = process.argv[2];
   const value = process.argv[3];
   const packageJson = jsonfile.readFileSync(packageJsonFilePath);
-  if (!packageJson.scripts) packageJson.scripts = {}
+  if (!packageJson.scripts) packageJson.scripts = {};
   if (packageJson.scripts[key]) {
     console.error('cuketractor script already exists');
   }
   packageJson.scripts[key] = value;
-  jsonfile.writeFileSync(packageJsonFilePath, packageJson, {spaces: 2});
+  jsonfile.writeFileSync(packageJsonFilePath, packageJson, { spaces: 2 });
 } catch (e) {
   if (e.message === 'ENOENT, no such file or directory \'package.json\'') {
-    throw new noPackageJsonExists();
+    throw new NoPackageJsonExists();
   } else {
     throw e;
   }
