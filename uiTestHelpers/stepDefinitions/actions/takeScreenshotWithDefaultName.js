489497969eba693212def9a1e05be9c2807a960d
Alex Rogers <arogers@linklaters.com>
2018-10-23 17:51:46 +0100

    fix lint

--- uiTestHelpers/stepDefinitions/actions/takeScreenshotWithDefaultName.js
+++ uiTestHelpers/stepDefinitions/actions/takeScreenshotWithDefaultName.js
@@ -1,7 +1,5 @@
 const takeScreenshot = require('./takeScreenshot');
 
 module.exports = function takeScreenshotWithDefaultName(callback) {
-  let bufferedImage;
-
   return takeScreenshot.call(this, '', callback);
 };
