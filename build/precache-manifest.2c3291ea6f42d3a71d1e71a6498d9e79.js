e693ab06dc2cdbdffb45d5de6f337f533486f399
Alex Rogers <alex.rogers.ctr@lastminute.com>
2018-11-04 19:25:10 +0000

    adding browserslist

---
+++ build/precache-manifest.2c3291ea6f42d3a71d1e71a6498d9e79.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e73f87984f2774d54301",
+    "url": "/static/js/2.e73f8798.chunk.js"
+  },
+  {
+    "revision": "49faf52c734582aaa1c1",
+    "url": "/static/css/main.641f471c.chunk.css"
+  },
+  {
+    "revision": "45606e48d6602b83c4f0",
+    "url": "/static/js/runtime~main.45606e48.js"
+  },
+  {
+    "revision": "f65268c5d201a0b38e9b",
+    "url": "/static/js/1.f65268c5.chunk.js"
+  },
+  {
+    "revision": "60234a088507c96265cd",
+    "url": "/static/js/4.60234a08.chunk.js"
+  },
+  {
+    "revision": "49faf52c734582aaa1c1",
+    "url": "/static/js/main.49faf52c.chunk.js"
+  },
+  {
+    "revision": "f2a5d5606bab009c4f61",
+    "url": "/static/js/3.f2a5d560.chunk.js"
+  },
+  {
+    "revision": "f2a5d5606bab009c4f61",
+    "url": "/static/css/3.3a9c9152.chunk.css"
+  },
+  {
+    "revision": "e73f87984f2774d54301",
+    "url": "/static/css/2.3a9c9152.chunk.css"
+  },
+  {
+    "revision": "f65268c5d201a0b38e9b",
+    "url": "/static/css/1.3a9c9152.chunk.css"
+  },
+  {
+    "revision": "59053caafa64bae56c43b1ae4afb668a",
+    "url": "/index.html"
+  }
+];
