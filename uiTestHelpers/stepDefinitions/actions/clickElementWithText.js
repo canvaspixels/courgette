fcf0c95c34c43b97dfe977766eff98dfa733cfc8
Alex Rogers <alex.rogers.ctr@lastminute.com>
2018-11-25 19:45:15 +0000

    fix for xpath in elementwithtext

--- uiTestHelpers/stepDefinitions/actions/clickElementWithText.js
+++ uiTestHelpers/stepDefinitions/actions/clickElementWithText.js
@@ -1,7 +1,7 @@
 module.exports = function clickElementWithText(nth, text) {
   const EC = protractor.ExpectedConditions;
 
-  let xpath = `//*[text()="${text}" or @value="${text}")]/ancestor-or-self::*[self::a or self::button or self::input]`;
+  let xpath = `//*[text()="${text}" or @value="${text}"]/ancestor-or-self::*[self::a or self::button or self::input]`;
   if (nth) {
     // remove nd from 2nd for example
     xpath = `(${xpath})[${nth.replace(/\D/g, '')}]`;
