571b6fe4ffeba762e046d09ae2ba2d06b86d4296
Alex Rogers <alex.rogers.ctr@lastminute.com>
2019-01-24 09:22:01 +0000

    adding docs folder

---
+++ docs/precache-manifest.09313444f8ecd10035a8fa131be264d1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e949837343bff8492190",
+    "url": "/static/js/3.e9498373.chunk.js"
+  },
+  {
+    "revision": "1047e8c5327b2d095e67",
+    "url": "/static/css/main.73ce57dd.chunk.css"
+  },
+  {
+    "revision": "d23e32bf1babbcdf8fdc",
+    "url": "/static/js/runtime~main.d23e32bf.js"
+  },
+  {
+    "revision": "4a0d21056b4f1488c6d4",
+    "url": "/static/js/1.4a0d2105.chunk.js"
+  },
+  {
+    "revision": "704328c610f6e2556811",
+    "url": "/static/js/6.704328c6.chunk.js"
+  },
+  {
+    "revision": "f6ff704f7d54d6382470",
+    "url": "/static/js/2.f6ff704f.chunk.js"
+  },
+  {
+    "revision": "95a44799d20c39a2548f",
+    "url": "/static/js/5.95a44799.chunk.js"
+  },
+  {
+    "revision": "1047e8c5327b2d095e67",
+    "url": "/static/js/main.1047e8c5.chunk.js"
+  },
+  {
+    "revision": "80387201296f3e869272",
+    "url": "/static/js/4.80387201.chunk.js"
+  },
+  {
+    "revision": "95a44799d20c39a2548f",
+    "url": "/static/css/5.6363fe8e.chunk.css"
+  },
+  {
+    "revision": "80387201296f3e869272",
+    "url": "/static/css/4.1238ad07.chunk.css"
+  },
+  {
+    "revision": "e949837343bff8492190",
+    "url": "/static/css/3.a9cc67c7.chunk.css"
+  },
+  {
+    "revision": "f6ff704f7d54d6382470",
+    "url": "/static/css/2.40780a8c.chunk.css"
+  },
+  {
+    "revision": "4a0d21056b4f1488c6d4",
+    "url": "/static/css/1.8a6c0009.chunk.css"
+  },
+  {
+    "revision": "9c5e192e0f22cdba197e4ad684e53677",
+    "url": "/index.html"
+  }
+];
