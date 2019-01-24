c2a560c914f5a0542362bd7a9ae8ad49a4b12c76
Alex Rogers <alex.rogers.ctr@lastminute.com>
2019-01-24 09:38:57 +0000

    committing docs folder to root

---
+++ precache-manifest.d5eef15ceeaae989f34e4a6702916292.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e949837343bff8492190",
+    "url": "/courgette/static/js/3.e9498373.chunk.js"
+  },
+  {
+    "revision": "1047e8c5327b2d095e67",
+    "url": "/courgette/static/css/main.73ce57dd.chunk.css"
+  },
+  {
+    "revision": "9205617c16fb9a3538ef",
+    "url": "/courgette/static/js/runtime~main.9205617c.js"
+  },
+  {
+    "revision": "4a0d21056b4f1488c6d4",
+    "url": "/courgette/static/js/1.4a0d2105.chunk.js"
+  },
+  {
+    "revision": "704328c610f6e2556811",
+    "url": "/courgette/static/js/6.704328c6.chunk.js"
+  },
+  {
+    "revision": "f6ff704f7d54d6382470",
+    "url": "/courgette/static/js/2.f6ff704f.chunk.js"
+  },
+  {
+    "revision": "95a44799d20c39a2548f",
+    "url": "/courgette/static/js/5.95a44799.chunk.js"
+  },
+  {
+    "revision": "1047e8c5327b2d095e67",
+    "url": "/courgette/static/js/main.1047e8c5.chunk.js"
+  },
+  {
+    "revision": "80387201296f3e869272",
+    "url": "/courgette/static/js/4.80387201.chunk.js"
+  },
+  {
+    "revision": "95a44799d20c39a2548f",
+    "url": "/courgette/static/css/5.6363fe8e.chunk.css"
+  },
+  {
+    "revision": "80387201296f3e869272",
+    "url": "/courgette/static/css/4.1238ad07.chunk.css"
+  },
+  {
+    "revision": "e949837343bff8492190",
+    "url": "/courgette/static/css/3.a9cc67c7.chunk.css"
+  },
+  {
+    "revision": "f6ff704f7d54d6382470",
+    "url": "/courgette/static/css/2.40780a8c.chunk.css"
+  },
+  {
+    "revision": "4a0d21056b4f1488c6d4",
+    "url": "/courgette/static/css/1.8a6c0009.chunk.css"
+  },
+  {
+    "revision": "ff395024f1f4c42161a3d93d952217aa",
+    "url": "/courgette/index.html"
+  }
+];
