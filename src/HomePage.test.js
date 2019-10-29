3f48c5f76621902c02b0a52337e39766fa3f892d
Alex Rogers <alex.rogers.ctr@lastminute.com>
2019-10-29 16:07:30 +0000

    deleting unused test app unit test

--- src/HomePage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HomePage from './HomePage';
-
-it('renders without crashing', () => {
-  const div = global.document.createElement('div');
-  ReactDOM.render(<HomePage />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
