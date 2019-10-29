0c7f4c2cca480678673887a72dcf5218ebc2d150
Gigi Minova <minova.gigi@gmail.com>
2019-10-29 15:55:17 +0000

    fix test

--- src/HomePage.test.js
+++ src/HomePage.test.js
@@ -1,9 +1,10 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './HomePage';
 
 it('renders without crashing', () => {
   const div = global.document.createElement('div');
-  ReactDOM.render(<HomePage />, div);
+  ReactDOM.render(<Router><HomePage /></Router>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
