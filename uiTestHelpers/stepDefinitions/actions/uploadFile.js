9d26cd9106c57692a19494a6199e43072d8c8336
Alex Rogers <cg@canvaspixels.com>
2020-05-05 05:47:26 +0100

    adding WDIO support - fix upload and change ci to use wdio

--- uiTestHelpers/stepDefinitions/actions/uploadFile.js
+++ uiTestHelpers/stepDefinitions/actions/uploadFile.js
@@ -1,15 +1,30 @@
 const path = require('path');
 
-module.exports = function uploadFile(fileToUpload, locatorKey) {
+module.exports = async function uploadFile(fileToUpload, locatorKey) {
   const currentPage = this.getCurrentPage();
   const cssSelector = currentPage.getSelectorFromLocatorKey(locatorKey);
+  
+  if (process.env.BINDINGS === 'WDIO') {
+    const elem = await $(cssSelector);
+
+    // todo add timeout
+    await elem.waitForDisplayed()
+    const absolutePath = path.resolve(fileToUpload);
+
+    browser.executeAsync((selector, callback) => {
+      document.querySelector(selector).style.display = 'inline';
+      callback();
+    }, cssSelector);
+
+    return elem.keys(absolutePath);
+  }
+
   return currentPage
     .getElementWhenInDOM(locatorKey)
     .then((el) => {
       const absolutePath = path.resolve(fileToUpload);
 
-      const executeFn = process.env.BINDINGS === 'WDIO' ? browser.executeAsync : browser.executeAsyncScript;
-      executeFn((selector, callback) => {
+      browser.executeAsyncScript((selector, callback) => {
         document.querySelector(selector).style.display = 'inline';
         callback();
       }, cssSelector);
