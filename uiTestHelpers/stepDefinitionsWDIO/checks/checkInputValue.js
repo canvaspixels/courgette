8c35406bb5c3437323b0e0f0c8c830dfb0366bbb
Alex Rogers <cg@canvaspixels.com>
2022-03-26 00:20:11 +0000

    working on formatter

--- uiTestHelpers/stepDefinitionsWDIO/checks/checkInputValue.js
+++ uiTestHelpers/stepDefinitionsWDIO/checks/checkInputValue.js
@@ -2,13 +2,12 @@ const path = require('path');
 
 module.exports = async function checkInputValue(locatorKey, isNot, expectedVal) {
   const pageObj = await this.getCurrentPage();
-  const el = await pageObj.getElement(locatorKey);
 
   if (process.env.COURGETTE_DEBUG) {
     console.log('In file:', path.basename(__filename), el);
   }
 
-  const value = await el.getAttribute('value');
+  const value = await pageObj.getElement(locatorKey).getAttribute('value');
 
   return isNot ?
     expect(value).to.not.equal(expectedVal) :
